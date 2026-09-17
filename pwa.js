247d3c85db174186774035f4610985c9e0025ef5
jottaavee <joaodev026@gmail.com>
2026-09-17 18:36:13 -0300

    tirando manuntençao

--- pwa.js
+++ pwa.js
@@ -17,8 +17,7 @@
     function isStandalone() {
         return (
             window.matchMedia &&
-            (window.matchMedia('(display-mode: standalone)').matches ||
-             window.matchMedia('(display-mode: fullscreen)').matches)
+            window.matchMedia('(display-mode: standalone)').matches
         ) || window.navigator.standalone === true;
     }
 
